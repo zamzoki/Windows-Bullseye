a9f62d8a61dcce686211af9518875be7dc0e02ee
David Zanoschi <david_z1980@yahoo.com>
2019-06-08 09:55:55 +0300

    firstCommit

---
+++ BullseyeCursors/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace BullseyeCursors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.targetPictureBox = new System.Windows.Forms.PictureBox();
+            this.attemptsLabel = new System.Windows.Forms.Label();
+            this.InstructionsLabel = new System.Windows.Forms.Label();
+            this.pointsLabel = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.xCursorPictureBox = new System.Windows.Forms.PictureBox();
+            this.xCursorTimer = new System.Windows.Forms.Timer(this.components);
+            this.yCursorTimer = new System.Windows.Forms.Timer(this.components);
+            this.yCursorPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.targetPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xCursorPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yCursorPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // targetPictureBox
+            // 
+            this.targetPictureBox.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.targetPictureBox.Location = new System.Drawing.Point(75, 65);
+            this.targetPictureBox.Name = "targetPictureBox";
+            this.targetPictureBox.Size = new System.Drawing.Size(400, 400);
+            this.targetPictureBox.TabIndex = 0;
+            this.targetPictureBox.TabStop = false;
+            // 
+            // attemptsLabel
+            // 
+            this.attemptsLabel.AutoSize = true;
+            this.attemptsLabel.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.attemptsLabel.Location = new System.Drawing.Point(94, 18);
+            this.attemptsLabel.Name = "attemptsLabel";
+            this.attemptsLabel.Size = new System.Drawing.Size(113, 31);
+            this.attemptsLabel.TabIndex = 1;
+            this.attemptsLabel.Text = "Attempts";
+            // 
+            // InstructionsLabel
+            // 
+            this.InstructionsLabel.AutoSize = true;
+            this.InstructionsLabel.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InstructionsLabel.Location = new System.Drawing.Point(12, 532);
+            this.InstructionsLabel.Name = "InstructionsLabel";
+            this.InstructionsLabel.Size = new System.Drawing.Size(40, 17);
+            this.InstructionsLabel.TabIndex = 2;
+            this.InstructionsLabel.Text = "EscR";
+            // 
+            // pointsLabel
+            // 
+            this.pointsLabel.AutoSize = true;
+            this.pointsLabel.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pointsLabel.Location = new System.Drawing.Point(309, 18);
+            this.pointsLabel.Name = "pointsLabel";
+            this.pointsLabel.Size = new System.Drawing.Size(82, 31);
+            this.pointsLabel.TabIndex = 3;
+            this.pointsLabel.Text = "Points";
+            // 
+            // timer1
+            // 
+            this.timer.Interval = 500;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // xCursorPictureBox
+            // 
+            this.xCursorPictureBox.Location = new System.Drawing.Point(75, 488);
+            this.xCursorPictureBox.Name = "xCursorPictureBox";
+            this.xCursorPictureBox.Size = new System.Drawing.Size(400, 10);
+            this.xCursorPictureBox.TabIndex = 4;
+            this.xCursorPictureBox.TabStop = false;
+            // 
+            // timerx
+            // 
+            this.xCursorTimer.Interval = 2;
+            this.xCursorTimer.Tick += new System.EventHandler(this.xCursorTimer_Tick);
+            // 
+            // timery
+            // 
+            this.yCursorTimer.Interval = 2;
+            this.yCursorTimer.Tick += new System.EventHandler(this.yCursorTimer_Tick);
+            // 
+            // yCursorPictureBox
+            // 
+            this.yCursorPictureBox.Location = new System.Drawing.Point(505, 65);
+            this.yCursorPictureBox.Name = "yCursorPictureBox";
+            this.yCursorPictureBox.Size = new System.Drawing.Size(10, 400);
+            this.yCursorPictureBox.TabIndex = 5;
+            this.yCursorPictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 591);
+            this.Controls.Add(this.yCursorPictureBox);
+            this.Controls.Add(this.xCursorPictureBox);
+            this.Controls.Add(this.pointsLabel);
+            this.Controls.Add(this.InstructionsLabel);
+            this.Controls.Add(this.attemptsLabel);
+            this.Controls.Add(this.targetPictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Bullseye";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.targetPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.xCursorPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yCursorPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox targetPictureBox;
+        private System.Windows.Forms.Label attemptsLabel;
+        private System.Windows.Forms.Label InstructionsLabel;
+        private System.Windows.Forms.Label pointsLabel;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.PictureBox xCursorPictureBox;
+        private System.Windows.Forms.Timer xCursorTimer;
+        private System.Windows.Forms.Timer yCursorTimer;
+        private System.Windows.Forms.PictureBox yCursorPictureBox;
+    }
+}
+
---
+++ BullseyeCursors/Form1.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+using System.Web;
+
+namespace BullseyeCursors
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // target variables
+        private static Bitmap targetBmp;
+        private Graphics targetGfx;
+
+        // x cursor variables
+        private static Bitmap xCursorBmp;
+        private Graphics xCursorGfx;
+        private int xCursorCoordinate = 0, xTemp = -10;
+
+        // y cursor variables
+        private static Bitmap yCursorBmp;
+        private Graphics yCursorGfx;
+        private int yCursorCoordinate = 0, yTemp = -10;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Attempts label
+            attemptsLabel.Location = new Point(50, 0);
+            attemptsLabel.Size = new Size(100, 50);
+            attemptsLabel.AutoSize = true;
+            attemptsLabel.Text = "Attempts:" + attempts.ToString();
+                       
+            //Points label
+            pointsLabel.Location = new Point(250, 0);
+            pointsLabel.Size = new Size(100, 50);
+            pointsLabel.AutoSize = true;
+            pointsLabel.Text = "Points:" + points.ToString();
+
+            // Instructions label
+            InstructionsLabel.AutoSize = true;
+            InstructionsLabel.Text = "Press Space to shoot.\nPress R to retry.\nPress Escape to exit.";
+
+            // Target graphics
+            targetBmp = new Bitmap(400, 400);
+            targetGfx = Graphics.FromImage(targetBmp);
+            targetGfx.FillEllipse(new SolidBrush(Color.DarkSlateGray), 0, 0, 400, 400);
+            targetGfx.FillEllipse(new SolidBrush(Color.DarkBlue), 50, 50, 300, 300);
+            targetGfx.FillEllipse(new SolidBrush(Color.Crimson), 100, 100, 200, 200);
+            targetGfx.FillEllipse(new SolidBrush(Color.Goldenrod), 150, 150, 100, 100);
+            targetGfx.FillEllipse(new SolidBrush(Color.ForestGreen), 190, 190, 20, 20);
+            targetPictureBox.Image = targetBmp;
+
+
+            // x cursor graphics
+            xCursorBmp = new Bitmap(400, 10);
+            xCursorGfx = Graphics.FromImage(xCursorBmp);
+            xCursorGfx.Clear(Color.LightGray);
+            xCursorPictureBox.Image = xCursorBmp;
+            xCursorTimer.Interval = 1;
+            xCursorTimer.Enabled = true;
+            xCursorTimer.Start();
+
+
+            // y cursor graphics
+            yCursorBmp = new Bitmap(10, 400);
+            yCursorGfx = Graphics.FromImage(yCursorBmp);
+            yCursorGfx.Clear(Color.LightGray);
+            yCursorPictureBox.Image = yCursorBmp;
+        }
+
+        
+        private void xCursorTimer_Tick(object sender, EventArgs e)
+        {
+            xCursorGfx.Clear(Color.LightGray);
+
+            if(xCursorCoordinate >= 0 && xCursorCoordinate <= 390 && xCursorCoordinate - xTemp > 0)
+            {
+                xCursorGfx.FillRectangle(new SolidBrush(Color.LightSeaGreen), xCursorCoordinate, 0, 10, 10);
+                xTemp = xCursorCoordinate;
+                xCursorCoordinate += 10;
+            }
+            else if(xCursorCoordinate == 400)
+            {
+                xTemp = xCursorCoordinate;
+                xCursorCoordinate -= 10;
+            }
+            else if (xCursorCoordinate >= 0 && xCursorCoordinate <= 390 && xCursorCoordinate - xTemp < 0)
+            {
+                xCursorGfx.FillRectangle(new SolidBrush(Color.LightSeaGreen), xCursorCoordinate, 0, 10, 10);
+                xTemp = xCursorCoordinate;
+                xCursorCoordinate -= 10;
+            }
+            else if (xCursorCoordinate == -10)
+            {
+                xTemp = xCursorCoordinate;
+                xCursorCoordinate += 10;
+            }
+
+            xCursorPictureBox.Image = xCursorBmp;
+        }
+
+
+        private void yCursorTimer_Tick(object sender, EventArgs e)
+        {
+            yCursorGfx.Clear(Color.LightGray);
+
+            if (yCursorCoordinate >= 0 && yCursorCoordinate <= 390 && yCursorCoordinate - yTemp > 0)
+            {
+                yCursorGfx.FillRectangle(new SolidBrush(Color.LightSeaGreen), 0, yCursorCoordinate, 10, 10);
+                yTemp = yCursorCoordinate;
+                yCursorCoordinate += 10;
+            }
+            else if (yCursorCoordinate == 400)
+            {
+                yTemp = yCursorCoordinate;
+                yCursorCoordinate -= 10;
+            }
+            else if (yCursorCoordinate >= 0 && yCursorCoordinate <= 390 && yCursorCoordinate - yTemp < 0)
+            {
+                yCursorGfx.FillRectangle(new SolidBrush(Color.LightSeaGreen), 0, yCursorCoordinate, 10, 10);
+                yTemp = yCursorCoordinate;
+                yCursorCoordinate -= 10;
+            }
+            else if (yCursorCoordinate == -10)
+            {
+                yTemp = yCursorCoordinate;
+                yCursorCoordinate += 10;
+            }
+            yCursorPictureBox.Image = yCursorBmp;
+        }
+
+        private int points = 0, attempts = 5;
+            private double centerX = 200;
+            private double centerY = 200;
+            private bool greenFlag = false, yellowFlag = false, redFlag = false,
+                            blueFlag = false, blackFlag = false;
+        private int xCoordinate = 0, yCoordinate = 0, spaceKeyPressedContor = 0;
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //KEYS ESCAPE
+            if ((Keys)e.KeyValue == Keys.Escape)
+            {
+                this.Close();
+            }
+            
+            //KEYS RETRY
+            if ((Keys)e.KeyValue == Keys.R)
+            {
+                targetGfx.FillEllipse(new SolidBrush(Color.DarkSlateGray), 0, 0, 400, 400);
+                targetGfx.FillEllipse(new SolidBrush(Color.DarkBlue), 50, 50, 300, 300);
+                targetGfx.FillEllipse(new SolidBrush(Color.Crimson), 100, 100, 200, 200);
+                targetGfx.FillEllipse(new SolidBrush(Color.Goldenrod), 150, 150, 100, 100);
+                targetGfx.FillEllipse(new SolidBrush(Color.ForestGreen), 190, 190, 20, 20);
+
+                targetPictureBox.Image = targetBmp;
+
+                points = 0;
+                pointsLabel.Text = "Points:" + points.ToString();
+
+                attempts = 5;
+                attemptsLabel.Text = "Attempts:" + attempts.ToString();
+
+                spaceKeyPressedContor = 0;
+                xCursorTimer.Enabled = true;
+                xCursorTimer.Start();
+                xCursorCoordinate = 0;
+                xTemp = -1;
+
+                yCursorCoordinate = 0;
+                yTemp = -1;
+            }
+
+            //KEYS SPACE
+            if((Keys)e.KeyValue == Keys.Space)
+            {
+                ++spaceKeyPressedContor;
+
+                if(attempts > 0 && spaceKeyPressedContor == 1)
+                {
+                    xCursorTimer.Stop();
+                    xCursorTimer.Enabled = false;
+                    if (xCursorCoordinate - xTemp >= 0)
+                        xCoordinate = xCursorCoordinate - 7;
+                    else if (xCursorCoordinate - xTemp < 0)
+                        xCoordinate = xCursorCoordinate + 14;
+                    xCursorPictureBox.Image = xCursorBmp;
+
+                    yCursorTimer.Interval = 1;
+                    yCursorTimer.Enabled = true;
+                    yCursorTimer.Start(); 
+                }
+
+                if (attempts > 0 && spaceKeyPressedContor == 2)
+                {
+                    yCursorTimer.Stop();
+                    yCursorTimer.Enabled = false;
+                    if (yCursorCoordinate - yTemp >= 0)
+                        yCoordinate = yCursorCoordinate - 7;
+                    else if (yCursorCoordinate - yTemp < 0)
+                        yCoordinate = yCursorCoordinate + 14;
+                    yCursorPictureBox.Image = yCursorBmp;
+                    
+                    timer.Enabled = true;
+                    timer.Start();
+                    timer.Interval = (750);
+
+                    // Green area 100p
+                    if (Math.Sqrt(Math.Pow(xCoordinate - centerX, 2f) + Math.Pow(yCoordinate - centerY, 2f)) < 10)
+                    {
+                        pointsLabel.Text = "Points:" + points.ToString() + "+100";
+                        attemptsLabel.Text = "Attempts:" + attempts.ToString() + "-1";
+
+                        greenFlag = true;
+
+                        targetGfx.FillEllipse(new SolidBrush(Color.White), xCoordinate - 4, yCoordinate - 4, 8, 8);
+                        targetPictureBox.Image = targetBmp;
+                    }
+
+                    // Yellow area 50p
+                    if ((Math.Sqrt(Math.Pow(xCoordinate - centerX, 2f) + Math.Pow(yCoordinate - centerY, 2f)) > 10) &&
+                        (Math.Sqrt(Math.Pow(xCoordinate - centerX, 2f) + Math.Pow(yCoordinate - centerY, 2f)) < 50))
+                    {
+                        pointsLabel.Text = "Points:" + points.ToString() + "+50";
+                        attemptsLabel.Text = "Attempts:" + attempts.ToString() + "-1";
+
+                        yellowFlag = true;
+
+                        targetGfx.FillEllipse(new SolidBrush(Color.White), xCoordinate - 4, yCoordinate - 4, 8, 8);
+                        targetPictureBox.Image = targetBmp;
+                    }
+
+                    // Red area 25p
+                    if ((Math.Sqrt(Math.Pow(xCoordinate - centerX, 2f) + Math.Pow(yCoordinate - centerY, 2f)) > 50) &&
+                        (Math.Sqrt(Math.Pow(xCoordinate - centerX, 2f) + Math.Pow(yCoordinate - centerY, 2f)) < 100))
+                    {
+                        pointsLabel.Text = "Points:" + points.ToString() + "+25";
+                        attemptsLabel.Text = "Attempts:" + attempts.ToString() + "-1";
+
+                        redFlag = true;
+
+                        targetGfx.FillEllipse(new SolidBrush(Color.White), xCoordinate - 4, yCoordinate - 4, 8, 8);
+                        targetPictureBox.Image = targetBmp;
+                    }
+
+                    // Blue area 10p
+                    if ((Math.Sqrt(Math.Pow(xCoordinate - centerX, 2f) + Math.Pow(yCoordinate - centerY, 2f)) > 100) &&
+                        (Math.Sqrt(Math.Pow(xCoordinate - centerX, 2f) + Math.Pow(yCoordinate - centerY, 2f)) < 150))
+                    {
+                        pointsLabel.Text = "Points:" + points.ToString() + "+10";
+                        attemptsLabel.Text = "Attempts:" + attempts.ToString() + "-1";
+
+                        blueFlag = true;
+
+                        targetGfx.FillEllipse(new SolidBrush(Color.White), xCoordinate - 4, yCoordinate - 4, 8, 8);
+                        targetPictureBox.Image = targetBmp;
+                    }
+
+                    // Black area 0p
+                    if ((Math.Sqrt(Math.Pow(xCoordinate - centerX, 2f) + Math.Pow(yCoordinate - centerY, 2f)) > 150) &&
+                        (Math.Sqrt(Math.Pow(xCoordinate - centerX, 2f) + Math.Pow(yCoordinate - centerY, 2f)) < 200))
+                    {
+                        pointsLabel.Text = "Points:" + points.ToString() + "+0";
+                        attemptsLabel.Text = "Attempts:" + attempts.ToString() + "-1";
+
+                        blackFlag = true;
+
+                        targetGfx.FillEllipse(new SolidBrush(Color.White), xCoordinate - 4, yCoordinate - 4, 8, 8);
+                        targetPictureBox.Image = targetBmp;
+                    }
+
+                    spaceKeyPressedContor = 0;
+                }
+            }
+        }
+        
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (greenFlag == true)
+            {
+                points += 100;
+                pointsLabel.Text = "Points:" + points.ToString();
+                --attempts;
+                attemptsLabel.Text = "Attempts:" + attempts.ToString();
+
+                greenFlag = false;
+            }
+
+            if (yellowFlag == true)
+            {
+                points += 50;
+                pointsLabel.Text = "Points:" + points.ToString();
+                --attempts;
+                attemptsLabel.Text = "Attempts:" + attempts.ToString();
+
+                yellowFlag = false;
+            }
+
+            if (redFlag == true)
+            {
+                points += 25;
+                pointsLabel.Text = "Points:" + points.ToString();
+                --attempts;
+                attemptsLabel.Text = "Attempts:" + attempts.ToString();
+
+                redFlag = false;
+            }
+
+            if (blueFlag == true)
+            {
+                points += 10;
+                pointsLabel.Text = "Points:" + points.ToString();
+                --attempts;
+                attemptsLabel.Text = "Attempts:" + attempts.ToString();
+
+                blueFlag = false;
+            }
+
+            if (blackFlag == true)
+            {
+                points += 0;
+                pointsLabel.Text = "Points:" + points.ToString();
+                --attempts;
+                attemptsLabel.Text = "Attempts:" + attempts.ToString();
+
+                blackFlag = false;
+            }
+            timer.Stop();
+            timer.Enabled = false;
+
+
+            if(attempts > 0)
+            {
+                spaceKeyPressedContor = 0;
+                xCursorTimer.Enabled = true;
+                xCursorTimer.Start();
+                xCursorCoordinate = 0;
+                xTemp = -1;
+
+                yCursorCoordinate = 0;
+                yTemp = -1;
+            }
+        }
+    }
+}
