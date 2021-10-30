03c989aea4c560114bf3a1643577b01e8ae1f089
David Zanoschi <david.zanoschi@endava.com>
2021-10-30 19:29:52 +0300

    remove warnings: extract string resources, remove redundant code

--- BullseyeCursors/Form1.cs
+++ BullseyeCursors/Form1.cs
@@ -1,14 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Net.Mail;
-using System.Web;
 
 namespace BullseyeCursors
 {
@@ -20,18 +12,18 @@ namespace BullseyeCursors
         }
 
         // Target variables
-        private static Bitmap targetBmp;
-        private Graphics targetGfx;
+        private static Bitmap _targetBitmap;
+        private Graphics targetGraphics;
 
         // x cursor variables
-        private static Bitmap xCursorBmp;
-        private Graphics xCursorGfx;
-        private int xCursorCoordinate = 0, xTemp = -10;
+        private static Bitmap _xCursorBitmap;
+        private Graphics xCursorGraphics;
+        private int xCursorCoordinate, xTemp = -10;
 
         // y cursor variables
-        private static Bitmap yCursorBmp;
-        private Graphics yCursorGfx;
-        private int yCursorCoordinate = 0, yTemp = -10;
+        private static Bitmap _yCursorBitmap;
+        private Graphics yCursorGraphics;
+        private int yCursorCoordinate, yTemp = -10;
 
         /// <summary>
         /// This method sets up the UI and visual elements of the application.
@@ -62,17 +54,17 @@ namespace BullseyeCursors
             attemptsLabel.Location = new Point(50, 0);
             attemptsLabel.Size = new Size(100, 50);
             attemptsLabel.AutoSize = true;
-            attemptsLabel.Text = "Attempts:" + attempts.ToString();
+            attemptsLabel.Text = StringResources.GetAttemptsText(attempts);
 
             //Points label
             pointsLabel.Location = new Point(250, 0);
             pointsLabel.Size = new Size(100, 50);
             pointsLabel.AutoSize = true;
-            pointsLabel.Text = "Points:" + points.ToString();
+            pointsLabel.Text = StringResources.GetPointsText(points);
 
             // Instructions label
             InstructionsLabel.AutoSize = true;
-            InstructionsLabel.Text = "Press Space to shoot.\nPress R to retry.\nPress Escape to exit.";
+            InstructionsLabel.Text = StringResources.GetInstructions();
         }
 
         /// <summary>
@@ -81,14 +73,14 @@ namespace BullseyeCursors
         private void DrawTarget()
         {
             
-            targetBmp = new Bitmap(400, 400);
-            targetGfx = Graphics.FromImage(targetBmp);
-            targetGfx.FillEllipse(new SolidBrush(Color.DarkSlateGray), 0, 0, 400, 400);
-            targetGfx.FillEllipse(new SolidBrush(Color.DarkBlue), 50, 50, 300, 300);
-            targetGfx.FillEllipse(new SolidBrush(Color.Crimson), 100, 100, 200, 200);
-            targetGfx.FillEllipse(new SolidBrush(Color.Goldenrod), 150, 150, 100, 100);
-            targetGfx.FillEllipse(new SolidBrush(Color.ForestGreen), 190, 190, 20, 20);
-            targetPictureBox.Image = targetBmp;
+            _targetBitmap = new Bitmap(400, 400);
+            targetGraphics = Graphics.FromImage(_targetBitmap);
+            targetGraphics.FillEllipse(new SolidBrush(Color.DarkSlateGray), 0, 0, 400, 400);
+            targetGraphics.FillEllipse(new SolidBrush(Color.DarkBlue), 50, 50, 300, 300);
+            targetGraphics.FillEllipse(new SolidBrush(Color.Crimson), 100, 100, 200, 200);
+            targetGraphics.FillEllipse(new SolidBrush(Color.Goldenrod), 150, 150, 100, 100);
+            targetGraphics.FillEllipse(new SolidBrush(Color.ForestGreen), 190, 190, 20, 20);
+            targetPictureBox.Image = _targetBitmap;
         }
 
         /// <summary>
@@ -97,10 +89,10 @@ namespace BullseyeCursors
         private void DrawXCursor()
         {
             // x cursor graphics
-            xCursorBmp = new Bitmap(400, 10);
-            xCursorGfx = Graphics.FromImage(xCursorBmp);
-            xCursorGfx.Clear(Color.LightGray);
-            xCursorPictureBox.Image = xCursorBmp;
+            _xCursorBitmap = new Bitmap(400, 10);
+            xCursorGraphics = Graphics.FromImage(_xCursorBitmap);
+            xCursorGraphics.Clear(Color.LightGray);
+            xCursorPictureBox.Image = _xCursorBitmap;
         }
 
         /// <summary>
@@ -109,19 +101,19 @@ namespace BullseyeCursors
         private void DrawYCursor()
         {
             // y cursor graphics
-            yCursorBmp = new Bitmap(10, 400);
-            yCursorGfx = Graphics.FromImage(yCursorBmp);
-            yCursorGfx.Clear(Color.LightGray);
-            yCursorPictureBox.Image = yCursorBmp;
+            _yCursorBitmap = new Bitmap(10, 400);
+            yCursorGraphics = Graphics.FromImage(_yCursorBitmap);
+            yCursorGraphics.Clear(Color.LightGray);
+            yCursorPictureBox.Image = _yCursorBitmap;
         }
 
         
 
         private void XCursorTimer_Tick(object sender, EventArgs e)
         {
-            xCursorGfx.Clear(Color.LightGray);
+            xCursorGraphics.Clear(Color.LightGray);
             XCursorMovement();
-            xCursorPictureBox.Image = xCursorBmp;
+            xCursorPictureBox.Image = _xCursorBitmap;
         }
 
         /// <summary>
@@ -131,7 +123,7 @@ namespace BullseyeCursors
         {
             if (xCursorCoordinate >= 0 && xCursorCoordinate <= 390 && xCursorCoordinate - xTemp > 0)
             {
-                xCursorGfx.FillRectangle(new SolidBrush(Color.LightSeaGreen), xCursorCoordinate, 0, 10, 10);
+                xCursorGraphics.FillRectangle(new SolidBrush(Color.LightSeaGreen), xCursorCoordinate, 0, 10, 10);
                 xTemp = xCursorCoordinate;
                 xCursorCoordinate += 10;
             }
@@ -142,7 +134,7 @@ namespace BullseyeCursors
             }
             else if (xCursorCoordinate >= 0 && xCursorCoordinate <= 390 && xCursorCoordinate - xTemp < 0)
             {
-                xCursorGfx.FillRectangle(new SolidBrush(Color.LightSeaGreen), xCursorCoordinate, 0, 10, 10);
+                xCursorGraphics.FillRectangle(new SolidBrush(Color.LightSeaGreen), xCursorCoordinate, 0, 10, 10);
                 xTemp = xCursorCoordinate;
                 xCursorCoordinate -= 10;
             }
@@ -155,9 +147,9 @@ namespace BullseyeCursors
 
         private void YCursorTimer_Tick(object sender, EventArgs e)
         {
-            yCursorGfx.Clear(Color.LightGray);
+            yCursorGraphics.Clear(Color.LightGray);
             YCursorMovement();
-            yCursorPictureBox.Image = yCursorBmp;
+            yCursorPictureBox.Image = _yCursorBitmap;
         }
 
         /// <summary>
@@ -167,7 +159,7 @@ namespace BullseyeCursors
         {
             if (yCursorCoordinate >= 0 && yCursorCoordinate <= 390 && yCursorCoordinate - yTemp > 0)
             {
-                yCursorGfx.FillRectangle(new SolidBrush(Color.LightSeaGreen), 0, yCursorCoordinate, 10, 10);
+                yCursorGraphics.FillRectangle(new SolidBrush(Color.LightSeaGreen), 0, yCursorCoordinate, 10, 10);
                 yTemp = yCursorCoordinate;
                 yCursorCoordinate += 10;
             }
@@ -178,7 +170,7 @@ namespace BullseyeCursors
             }
             else if (yCursorCoordinate >= 0 && yCursorCoordinate <= 390 && yCursorCoordinate - yTemp < 0)
             {
-                yCursorGfx.FillRectangle(new SolidBrush(Color.LightSeaGreen), 0, yCursorCoordinate, 10, 10);
+                yCursorGraphics.FillRectangle(new SolidBrush(Color.LightSeaGreen), 0, yCursorCoordinate, 10, 10);
                 yTemp = yCursorCoordinate;
                 yCursorCoordinate -= 10;
             }
@@ -189,11 +181,21 @@ namespace BullseyeCursors
             }
         }
 
-        private int points = 0, attempts = 5;
-        private readonly double centerX = 200;
-        private readonly double centerY = 200;
-        private bool greenFlag = false, yellowFlag = false, redFlag = false, blueFlag = false, blackFlag = false;
-        private int xCoordinate = 0, yCoordinate = 0, spaceKeyPressedContor = 0;
+        private int points;
+        private int attempts = 5;
+            
+        private const double CenterX = 200;
+        private const double CenterY = 200;
+        
+        private bool greenFlag;
+        private bool yellowFlag;
+        private bool redFlag;
+        private bool blueFlag;
+        private bool blackFlag;
+
+        private int xCoordinate;
+        private int yCoordinate;
+        private int spaceKeyPressedCounter;
 
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
@@ -207,21 +209,21 @@ namespace BullseyeCursors
             // RETRY
             if ((Keys)e.KeyValue == Keys.R)
             {
-                targetGfx.FillEllipse(new SolidBrush(Color.DarkSlateGray), 0, 0, 400, 400);
-                targetGfx.FillEllipse(new SolidBrush(Color.DarkBlue), 50, 50, 300, 300);
-                targetGfx.FillEllipse(new SolidBrush(Color.Crimson), 100, 100, 200, 200);
-                targetGfx.FillEllipse(new SolidBrush(Color.Goldenrod), 150, 150, 100, 100);
-                targetGfx.FillEllipse(new SolidBrush(Color.ForestGreen), 190, 190, 20, 20);
+                targetGraphics.FillEllipse(new SolidBrush(Color.DarkSlateGray), 0, 0, 400, 400);
+                targetGraphics.FillEllipse(new SolidBrush(Color.DarkBlue), 50, 50, 300, 300);
+                targetGraphics.FillEllipse(new SolidBrush(Color.Crimson), 100, 100, 200, 200);
+                targetGraphics.FillEllipse(new SolidBrush(Color.Goldenrod), 150, 150, 100, 100);
+                targetGraphics.FillEllipse(new SolidBrush(Color.ForestGreen), 190, 190, 20, 20);
 
-                targetPictureBox.Image = targetBmp;
+                targetPictureBox.Image = _targetBitmap;
 
                 points = 0;
-                pointsLabel.Text = "Points:" + points.ToString();
+                pointsLabel.Text = StringResources.GetPointsText(points);
 
                 attempts = 5;
-                attemptsLabel.Text = "Attempts:" + attempts.ToString();
+                attemptsLabel.Text = StringResources.GetAttemptsText(attempts);
 
-                spaceKeyPressedContor = 0;
+                spaceKeyPressedCounter = 0;
                 xCursorTimer.Enabled = true;
                 xCursorTimer.Start();
                 xCursorCoordinate = 0;
@@ -234,9 +236,9 @@ namespace BullseyeCursors
             // SPACE
             if((Keys)e.KeyValue == Keys.Space)
             {
-                ++spaceKeyPressedContor;
+                ++spaceKeyPressedCounter;
 
-                if(attempts > 0 && spaceKeyPressedContor == 1)
+                if(attempts > 0 && spaceKeyPressedCounter == 1)
                 {
                     xCursorTimer.Stop();
                     xCursorTimer.Enabled = false;
@@ -244,14 +246,14 @@ namespace BullseyeCursors
                         xCoordinate = xCursorCoordinate - 7;
                     else if (xCursorCoordinate - xTemp < 0)
                         xCoordinate = xCursorCoordinate + 14;
-                    xCursorPictureBox.Image = xCursorBmp;
+                    xCursorPictureBox.Image = _xCursorBitmap;
 
                     yCursorTimer.Interval = 1;
                     yCursorTimer.Enabled = true;
                     yCursorTimer.Start(); 
                 }
 
-                if (attempts > 0 && spaceKeyPressedContor == 2)
+                if (attempts > 0 && spaceKeyPressedCounter == 2)
                 {
                     yCursorTimer.Stop();
                     yCursorTimer.Enabled = false;
@@ -259,67 +261,59 @@ namespace BullseyeCursors
                         yCoordinate = yCursorCoordinate - 7;
                     else if (yCursorCoordinate - yTemp < 0)
                         yCoordinate = yCursorCoordinate + 14;
-                    yCursorPictureBox.Image = yCursorBmp;
+                    yCursorPictureBox.Image = _yCursorBitmap;
                     
                     timer.Enabled = true;
                     timer.Start();
                     timer.Interval = (750);
 
+                    var pointsToAdd = 0;
                     // Green area 100p
-                    if (Math.Sqrt(Math.Pow(xCoordinate - centerX, 2f) + Math.Pow(yCoordinate - centerY, 2f)) < 10)
+                    if (Math.Sqrt(Math.Pow(xCoordinate - CenterX, 2f) + Math.Pow(yCoordinate - CenterY, 2f)) < 10)
                     {
-                        pointsLabel.Text = "Points:" + points.ToString() + "+100";
-                        attemptsLabel.Text = "Attempts:" + attempts.ToString() + "-1";
-
+                        pointsToAdd = 100;
                         greenFlag = true;
                         DrawTargetHole();
                     }
 
                     // Yellow area 50p
-                    if ((Math.Sqrt(Math.Pow(xCoordinate - centerX, 2f) + Math.Pow(yCoordinate - centerY, 2f)) > 10) &&
-                        (Math.Sqrt(Math.Pow(xCoordinate - centerX, 2f) + Math.Pow(yCoordinate - centerY, 2f)) < 50))
+                    if ((Math.Sqrt(Math.Pow(xCoordinate - CenterX, 2f) + Math.Pow(yCoordinate - CenterY, 2f)) > 10) &&
+                        (Math.Sqrt(Math.Pow(xCoordinate - CenterX, 2f) + Math.Pow(yCoordinate - CenterY, 2f)) < 50))
                     {
-                        pointsLabel.Text = "Points:" + points.ToString() + "+50";
-                        attemptsLabel.Text = "Attempts:" + attempts.ToString() + "-1";
-
+                        pointsToAdd = 50;
                         yellowFlag = true;
                         DrawTargetHole();
                     }
 
                     // Red area 25p
-                    if ((Math.Sqrt(Math.Pow(xCoordinate - centerX, 2f) + Math.Pow(yCoordinate - centerY, 2f)) > 50) &&
-                        (Math.Sqrt(Math.Pow(xCoordinate - centerX, 2f) + Math.Pow(yCoordinate - centerY, 2f)) < 100))
+                    if ((Math.Sqrt(Math.Pow(xCoordinate - CenterX, 2f) + Math.Pow(yCoordinate - CenterY, 2f)) > 50) &&
+                        (Math.Sqrt(Math.Pow(xCoordinate - CenterX, 2f) + Math.Pow(yCoordinate - CenterY, 2f)) < 100))
                     {
-                        pointsLabel.Text = "Points:" + points.ToString() + "+25";
-                        attemptsLabel.Text = "Attempts:" + attempts.ToString() + "-1";
-
+                        pointsToAdd = 25;
                         redFlag = true;
                         DrawTargetHole();
                     }
 
                     // Blue area 10p
-                    if ((Math.Sqrt(Math.Pow(xCoordinate - centerX, 2f) + Math.Pow(yCoordinate - centerY, 2f)) > 100) &&
-                        (Math.Sqrt(Math.Pow(xCoordinate - centerX, 2f) + Math.Pow(yCoordinate - centerY, 2f)) < 150))
+                    if ((Math.Sqrt(Math.Pow(xCoordinate - CenterX, 2f) + Math.Pow(yCoordinate - CenterY, 2f)) > 100) &&
+                        (Math.Sqrt(Math.Pow(xCoordinate - CenterX, 2f) + Math.Pow(yCoordinate - CenterY, 2f)) < 150))
                     {
-                        pointsLabel.Text = "Points:" + points.ToString() + "+10";
-                        attemptsLabel.Text = "Attempts:" + attempts.ToString() + "-1";
-
+                        pointsToAdd = 10;
                         blueFlag = true;
                         DrawTargetHole();
                     }
 
                     // Black area 0p
-                    if ((Math.Sqrt(Math.Pow(xCoordinate - centerX, 2f) + Math.Pow(yCoordinate - centerY, 2f)) > 150) &&
-                        (Math.Sqrt(Math.Pow(xCoordinate - centerX, 2f) + Math.Pow(yCoordinate - centerY, 2f)) < 200))
+                    if ((Math.Sqrt(Math.Pow(xCoordinate - CenterX, 2f) + Math.Pow(yCoordinate - CenterY, 2f)) > 150) &&
+                        (Math.Sqrt(Math.Pow(xCoordinate - CenterX, 2f) + Math.Pow(yCoordinate - CenterY, 2f)) < 200))
                     {
-                        pointsLabel.Text = "Points:" + points.ToString() + "+0";
-                        attemptsLabel.Text = "Attempts:" + attempts.ToString() + "-1";
-
                         blackFlag = true;
                         DrawTargetHole();
                     }
-
-                    spaceKeyPressedContor = 0;
+                    
+                    pointsLabel.Text = StringResources.GetPointsWithPointsToAddText(points, pointsToAdd);
+                    attemptsLabel.Text = StringResources.GetAttemptsWithMinusOneText(attempts);
+                    spaceKeyPressedCounter = 0;
                 }
             }
         }
@@ -329,8 +323,8 @@ namespace BullseyeCursors
         /// </summary>
         private void DrawTargetHole()
         {
-            targetGfx.FillEllipse(new SolidBrush(Color.White), xCoordinate - 4, yCoordinate - 4, 8, 8);
-            targetPictureBox.Image = targetBmp;
+            targetGraphics.FillEllipse(new SolidBrush(Color.White), xCoordinate - 4, yCoordinate - 4, 8, 8);
+            targetPictureBox.Image = _targetBitmap;
         }
 
         /// <summary>
@@ -341,62 +335,47 @@ namespace BullseyeCursors
         /// <param name="e"></param>
         private void Timer_Tick(object sender, EventArgs e)
         {
-            if (greenFlag == true)
+            if (greenFlag)
             {
                 points += 100;
-                pointsLabel.Text = "Points:" + points.ToString();
-                --attempts;
-                attemptsLabel.Text = "Attempts:" + attempts.ToString();
-
                 greenFlag = false;
             }
 
-            if (yellowFlag == true)
+            if (yellowFlag)
             {
                 points += 50;
-                pointsLabel.Text = "Points:" + points.ToString();
-                --attempts;
-                attemptsLabel.Text = "Attempts:" + attempts.ToString();
-
                 yellowFlag = false;
             }
 
-            if (redFlag == true)
+            if (redFlag)
             {
                 points += 25;
-                pointsLabel.Text = "Points:" + points.ToString();
-                --attempts;
-                attemptsLabel.Text = "Attempts:" + attempts.ToString();
-
                 redFlag = false;
             }
 
-            if (blueFlag == true)
+            if (blueFlag)
             {
                 points += 10;
-                pointsLabel.Text = "Points:" + points.ToString();
-                --attempts;
-                attemptsLabel.Text = "Attempts:" + attempts.ToString();
-
                 blueFlag = false;
             }
 
-            if (blackFlag == true)
+            if (blackFlag)
             {
                 points += 0;
-                pointsLabel.Text = "Points:" + points.ToString();
-                --attempts;
-                attemptsLabel.Text = "Attempts:" + attempts.ToString();
-
                 blackFlag = false;
             }
+            
+            --attempts;
+            pointsLabel.Text = StringResources.GetPointsText(points);
+            attemptsLabel.Text = StringResources.GetAttemptsText(attempts);
+            
             timer.Stop();
             timer.Enabled = false;
 
 
             if(attempts > 0)
             {
-                spaceKeyPressedContor = 0;
+                spaceKeyPressedCounter = 0;
                 xCursorTimer.Enabled = true;
                 xCursorTimer.Start();
                 xCursorCoordinate = 0;
---
+++ BullseyeCursors/StringResources.cs
@@ -0,0 +1,30 @@
+﻿
+internal static class StringResources
+{
+    private const string ShootInstruction = "Press [Space] to shoot.";
+
+    private const string RetryInstruction = "Press [R] to retry.";
+    
+    private const string ExitInstruction = "Press [Esc] to exit.";
+    
+    private const string AttemptsLabel = "Attempts: ";
+
+    private const string PointsLabel = "Points";
+
+    private const string MinusOne = "-1";
+
+    public static string GetInstructions()
+        => $"{ShootInstruction}\n{RetryInstruction}\n{ExitInstruction}";
+
+    public static string GetPointsText(int noOfPoints)
+        => $"{PointsLabel}: {noOfPoints}";
+    
+    public static string GetPointsWithPointsToAddText(int noOfPoints, int noOfPointsToAdd)
+        => $"{PointsLabel}: {noOfPoints}+{noOfPointsToAdd}"; 
+
+    public static string GetAttemptsText(int noOfAttempts)
+        => $"{AttemptsLabel}: {noOfAttempts}";
+
+    public static string GetAttemptsWithMinusOneText(int noOfAttempts)
+        => $"{AttemptsLabel}: {noOfAttempts}{MinusOne}";
+}
